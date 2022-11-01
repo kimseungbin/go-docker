d73411a8342934f036cf56f3fefcad54a24e13a5
SeungBin Kim <seungbin0508@gmail.com>
2022-11-01 15:38:27 +0900

    feat: add /database path

--- src/main.go
+++ src/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
+	"github.com/go-redis/redis/v9"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"net/http"
 	"os"
 )
+
+var ctx = context.Background()
 
 func main() {
 	e := echo.New()
@@ -19,6 +23,21 @@ func main() {
 
 	e.GET("/ping", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
+	})
+
+	e.GET("/database", func(c echo.Context) error {
+		rdb := redis.NewClient(&redis.Options{
+			Addr:     "redis:6379",
+			Password: "",
+			DB:       0,
+		})
+
+		result, err := rdb.Ping(ctx).Result()
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, struct{ Status string }{Status: err.Error()})
+
+		}
+		return c.JSON(http.StatusOK, struct{ Status string }{Status: result})
 	})
 
 	httpPort := os.Getenv("HTTP_PORT")
